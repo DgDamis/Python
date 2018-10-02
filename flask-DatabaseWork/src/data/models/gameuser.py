dcce6ef11c95bd2a8707598ee465a2e250b1947e
DgDamis <adam.smehyl@seznam.cz>
2018-10-02 23:26:09 +0200

    Added DatabaseWork

---
+++ flask-DatabaseWork/src/data/models/gameuser.py
@@ -0,0 +1,55 @@
+from flask_login import UserMixin
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Boolean, Integer, String
+
+from ..database import db
+from ..mixins import CRUDModel
+from ..util import generate_random_token
+from ...settings import app_config
+from ...extensions import bcrypt
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Boolean, Integer, String, Date,Float
+
+from ..database import db
+from ..mixins import CRUDModel
+
+class GameUser(CRUDModel):
+    __tablename__ = 'gameusers'
+    __table_args__ = {'sqlite_autoincrement': True}
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(64), nullable=False, unique=True, index=True, doc="The user's email address.")
+    username = Column(String(64), nullable=False, unique=True, index=True, doc="The user's username.")
+    datumprvnihopristupu = Column(Date)
+
+
+    # Use custom constructor
+    # pylint: disable=W0231
+    def __init__(self, **kwargs):
+        self.activate_token = generate_random_token()
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    @staticmethod
+    def find_by_email(email):
+        return db.session.query(User).filter_by(email=email).scalar()
+
+    @staticmethod
+    def find_by_username(username):
+        return db.session.query(User).filter_by(username=username).scalar()
+
+    # pylint: disable=R0201
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password, app_config.BCRYPT_LOG_ROUNDS)
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    def is_verified(self):
+        " Returns whether a user has verified their email "
+        return self.verified is True
---
+++ flask-DatabaseWork/src/public/views.py
@@ -0,0 +1,62 @@
+"""
+Logic for dashboard related routes
+"""
+from flask import Blueprint, render_template, flash
+from .forms import LogUserForm, secti,masoform, VstupForm, gameuserForm
+from ..data.database import db
+from ..data.models import LogUser, GameUser
+blueprint = Blueprint('public', __name__)
+
+@blueprint.route('/', methods=['GET'])
+def index():
+    return render_template('public/index.tmpl')
+
+@blueprint.route('/vstup', methods=['GET','POST'])
+def vstup():
+    form = VstupForm()
+    if form.validate_on_submit():
+        return render_template('public/vystup2.tmpl', ks=form.ks.data, cena=form.cena.data, DPH=form.DPH.data,
+                               celkem=float(form.cena.data)*float(form.ks.data)*float((float(form.DPH.data)/100)+1))
+    return render_template('public/vstup.tmpl',form = form)
+
+@blueprint.route('/loguserinput',methods=['GET', 'POST'])
+def InsertLogUser():
+    form = LogUserForm()
+    if form.validate_on_submit():
+        LogUser.create(**form.data)
+    return render_template("public/LogUser.tmpl", form=form)
+
+@blueprint.route('/loguserlist',methods=['GET'])
+def ListuserLog():
+    pole = db.session.query(LogUser).all()
+    return render_template("public/listuser.tmpl",data = pole)
+
+@blueprint.route('/secti', methods=['GET','POST'])
+def scitani():
+    form = secti()
+    if form.validate_on_submit():
+        return render_template('public/vystup.tmpl',hod1=form.hodnota1.data,hod2=form.hodnota2.data,suma=form.hodnota1.data+form.hodnota2.data)
+    return render_template('public/secti.tmpl', form=form)
+
+@blueprint.route('/maso', methods=['GET','POST'])
+def masof():
+    form = masoform()
+    if form.validate_on_submit():
+        return render_template('public/masovystup.tmpl',hod1=form.hodnota1.data,hod2=form.hodnota2.data,suma=form.hodnota1.data+form.hodnota2.data)
+    return render_template('public/maso.tmpl', form=form)
+
+@blueprint.route('/newgameuser', methods=['GET','POST'])
+def gameuseredit():
+    form = gameuserForm()
+    if form.validate_on_submit():
+        new_gameuser = GameUser.create(**form.data)
+    return render_template('public/gameuser.tmpl', form=form)
+
+@blueprint.route('/gameuseredit/<int:id>', methods=['GET','POST'])
+def gameusere(id):
+    user = db.session.query(GameUser).get(id)
+    form = gameuserForm(obj=user)
+    if form.validate_on_submit():
+        user.update(**form.data)
+        flash("Ulozeno",category="info")
+    return render_template('public/gameuser.tmpl', form=form)
